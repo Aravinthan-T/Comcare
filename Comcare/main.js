5192e2c7cd38be95ede3fd0c74d892cc52212f3f
Aravinthan-T <57287354+Aravinthan-T@users.noreply.github.com>
2020-01-26 12:16:55 +0530

    Add files via upload

---
+++ Comcare/main.js
@@ -0,0 +1,18 @@
+// card carousel
+
+
+$(document).ready(function () {
+  console.log("about to in");
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  });
+
+// card quote
+
+
